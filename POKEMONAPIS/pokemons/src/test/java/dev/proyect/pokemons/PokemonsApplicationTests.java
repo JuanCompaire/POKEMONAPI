7629039b6457f33420bfa8643e60ff832c77b492
JuanCompaire <juancompaire2004@gmail.com>
2023-12-19 14:54:18 +0100

    importamos proyecto

---
+++ POKEMONAPIS/pokemons/src/test/java/dev/proyect/pokemons/PokemonsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.proyect.pokemons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
